67a2d3fdaec952bbc6fc6ce5f163d4e541e127f7
Ahmadharmoush-1 <ahmadharmoush74@gmail.com>
2025-10-24 22:23:13 +0300

    Prepared backend for Render deployment

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://senior-tnop.onrender.com/api/auth";
+const API_URL = "https://senior-tnop.onrender.com/api/auth/test";
 
 export const register = async (name: string, email: string, password: string) => {
   const res = await axios.post(`${API_URL}/register`, { name, email, password });
